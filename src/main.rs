e558299fa70128696d924ad9bcb85326a34ce378
cyb3rloud <macgeorgendungu@gmail.com>
2026-01-03 00:41:32 +0300

    print error message for invalid commands

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
      print!("$ ");
      io::stdout().flush().unwrap();
+
+
+     //task 2:print error messages for invalid commands
+
+     let mut command = String::new();
+     io::stdin().read_line(&mut command).unwrap();
+     //now let's print command not dound message
+     println!("{}: command not found", command.trim());
 }
