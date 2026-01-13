2b0512b8ff24d94e44bbcf3a04f000e0d10b9376
cyb3rloud <macgeorgendungu@gmail.com>
2026-01-13 17:23:18 +0300

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,26 @@ fn main() {
 
      let mut command = String::new();
      io::stdin().read_line(&mut command).unwrap();
-     //now let's print command not dound message
+     //now let's print command not found message
      println!("{}: command not found", command.trim());
-}
+
+
+     //task 3: implement a REPL loop
+      loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command).unwrap();
+        //trim the command to remove whitespace and newline characters
+
+        let command = command.trim();
+        if command.is_empty() {
+            continue; // skip empty commands
+        }
+        println!("{}: command not found", command);
+    }
+
+
+    }
