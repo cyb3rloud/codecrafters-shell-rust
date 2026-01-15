e7dcfd90d522b02004af2650bb65c32692203539
cyb3rloud <macgeorgendungu@gmail.com>
2026-01-15 18:55:26 +0300

    Exit the Loop

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,7 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-     print!("$ ");
-     io::stdout().flush().unwrap();
-
-
-     //task 2:print error messages for invalid commands
-
-     let mut command = String::new();
-     io::stdin().read_line(&mut command).unwrap();
-     //now let's print command not found message
-     println!("{}: command not found", command.trim());
-
-
-     //task 3: implement a REPL loop
+  
       loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -29,8 +16,14 @@ fn main() {
         if command.is_empty() {
             continue; // skip empty commands
         }
+        else if command == "exit" {
+            break;
+        }
+        else {     
+       
         println!("{}: command not found", command);
     }
 
-
+      }
     }
+    
