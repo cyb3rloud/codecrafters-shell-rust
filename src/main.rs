28b3bd6716cc57a2f03d04928f11d87e490fc4a8
cyb3rloud <macgeorgendungu@gmail.com>
2026-01-22 23:47:32 +0300

    Implement echo

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+
+
+
+
+
+fn handle_echo(args: &[&str]) {
+             //now, we join the arguments back together with spaces and print them
+             println!("{}", args.join(" "));   
+            }
+
 fn main() {
+
+   
+
+
   
       loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         
-        let mut command = String::new();
+        let mut input = String::new();
 
-        io::stdin().read_line(&mut command).unwrap();
+        io::stdin().read_line(&mut input).unwrap();
         //trim the command to remove whitespace and newline characters
 
-        let command = command.trim();
-        if command.is_empty() {
-            continue; // skip empty commands
-        }
-        else if command == "exit" {
-            break;
-        }
-        else {     
-       
-        println!("{}: command not found", command);
+    let parts: Vec<&str> = input.split_whitespace().collect();
+    if parts.is_empty() { continue; }
+
+
+    let command = parts[0];
+    let args = &parts[1..];
+
+
+    match command {
+        "exit" | "bye" => break,
+        "echo" => handle_echo(args),
+        _ => println!("{}: command not found", command)
     }
 
-      }
+}
+       
+        
     }
+
+
     
